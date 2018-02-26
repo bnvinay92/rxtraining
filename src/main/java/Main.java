ad22c4b85bc88f54cb089ecf7c3c7c1f88706657
Vinay Nagaraj <bnvinay92@gmail.com>
2018-02-26 23:46:44 +0530

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+// Building a promise-like library
+// Scope:
+// - Represent async computations
+// - Provide an easy way to schedule these computations
+// - Provide a way to combine async operations
+
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
